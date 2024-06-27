778898e5bba43fd680f43f41a46b5352e4458c6d
amaify2 <tobe@trufin.io>
2024-06-27 14:11:11 +0100

    feat: Upgrade server-side auth

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,31 +1,23 @@
-import type { CookieOptions } from "@supabase/ssr";
 import { createServerClient } from "@supabase/ssr";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from "@/helpers/constants";
 
-export function createClient(request: NextRequest) {
-  let response = NextResponse.next({ request: { headers: request.headers } });
+export async function updateSession(request: NextRequest) {
+  let supabaseResponse = NextResponse.next({ request });
 
-  const supabase = createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     cookies: {
-      get(name: string) {
-        return request.cookies.get(name)?.value;
+      getAll() {
+        return request.cookies.getAll();
       },
-      set(name: string, value: string, options: CookieOptions) {
-        // If the cookie is updated, update the cookies for the request and response
-        request.cookies.set({ name, value, ...options });
-        response = NextResponse.next({ request: { headers: request.headers } });
-        response.cookies.set({ name, value, ...options });
-      },
-      remove(name: string, options: CookieOptions) {
-        // If the cookie is removed, update the cookies for the request and response
-        request.cookies.set({ name, value: "", ...options });
-        response = NextResponse.next({ request: { headers: request.headers } });
-        response.cookies.set({ name, value: "", ...options });
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
+        supabaseResponse = NextResponse.next({ request });
+        cookiesToSet.forEach(({ name, value, options }) => supabaseResponse.cookies.set(name, value, options));
       }
     }
   });
 
-  return { supabase, response };
+  return supabaseResponse;
 }
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,27 +1,40 @@
-import { type CookieOptions, createServerClient } from "@supabase/ssr";
-import type { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from "@/helpers/constants";
 
-export function createClient(cookieStore: ReturnType<typeof cookies>) {
+export function createClient() {
+  const cookieStore = cookies();
+
   return createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     cookies: {
-      get(name: string) {
-        return cookieStore.get(name)?.value;
+      getAll() {
+        return cookieStore.getAll();
       },
-      set(name: string, value: string, options: CookieOptions) {
-        try {
-          cookieStore.set({ name, value, ...options });
-        } catch (error) {
-          console.info("Could not set cookie", name);
-        }
-      },
-      remove(name: string, options: CookieOptions) {
-        try {
-          cookieStore.set({ name, value: "", ...options });
-        } catch (error) {
-          console.info("Could not remove cookie", name);
-        }
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options));
       }
     }
   });
+
+  // return createServerClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  //   cookies: {
+  //     get(name: string) {
+  //       return cookieStore.get(name)?.value;
+  //     },
+  //     set(name: string, value: string, options: CookieOptions) {
+  //       try {
+  //         cookieStore.set({ name, value, ...options });
+  //       } catch (error) {
+  //         console.info("Could not set cookie", name);
+  //       }
+  //     },
+  //     remove(name: string, options: CookieOptions) {
+  //       try {
+  //         cookieStore.set({ name, value: "", ...options });
+  //       } catch (error) {
+  //         console.info("Could not remove cookie", name);
+  //       }
+  //     }
+  //   }
+  // });
 }
